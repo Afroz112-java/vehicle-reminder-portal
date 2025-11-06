283f8f259b5e8f14fabe6c9d87ce98cbec00c7a5
V.Rajasekhar <rajasekharvanjeti03@gmail.com>
2025-11-06 10:11:52 +0530

    first commit

--- src/main/java/com/example/vehicle/entity/VehicleEntity.java
+++ src/main/java/com/example/vehicle/entity/VehicleEntity.java
@@ -1,6 +1,27 @@
 package com.example.vehicle.entity;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "vehicles")
+
 public class VehicleEntity {
-    private int id;
-    private int vehicleId;
+
+    @Id
+    @GeneratedValue(stratergy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String regNumber;
+    private String brand;
+    private String model;
+    private LocalDate insuranceExpiryDate;
+    private LocalDate serviceDueDate;
+    private String User;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private boolean active = true;
+
 }
