f504bd970e3b34d73175449b63038e495e4d751b
Teja <NARASIMHA@PRADEEP-NAGINENI>
2025-11-06 16:14:41 +0530

    I write user Entity.

--- src/main/java/com/example/vehicle/entity/VehicleEntity.java
+++ src/main/java/com/example/vehicle/entity/VehicleEntity.java
@@ -1,4 +1,32 @@
 package com.example.vehicle.entity;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "users")
 
 public class VehicleEntity {
-}
+              @Id
+                @GeneratedValue(strategy = GenerationType.IDENTITY)
+                private Long id;
+                private String name;
+                private String email;
+                private String password;
+                private String phone;
+                private String role; // instead of enum, just plain string for now
+                private LocalDateTime createdAt;
+                private LocalDateTime updatedAt;
+
+             // @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+              //private List<Vehicle> vehicles;
+        }
+
+
+
+
+
+
+
+
