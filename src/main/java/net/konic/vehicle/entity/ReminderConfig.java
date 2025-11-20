56ed45a558faaa8854c61cca4c3d25cca405eb64
Shaik Chand Basha <skchandbasha2023@gmail.com>
2025-11-20 12:33:07 +0530

    style: format code by adding blank lines for improved readability

--- src/main/java/net/konic/vehicle/entity/ReminderConfig.java
+++ src/main/java/net/konic/vehicle/entity/ReminderConfig.java
@@ -7,6 +7,7 @@ import lombok.Data;
 @Data
 public class ReminderConfig {
 
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- src/main/java/net/konic/vehicle/repository/ReminderConfigRepository.java
+++ src/main/java/net/konic/vehicle/repository/ReminderConfigRepository.java
@@ -4,4 +4,6 @@ import net.konic.vehicle.entity.ReminderConfig;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReminderConfigRepository extends JpaRepository<ReminderConfig, Long> {
+
+
 }
